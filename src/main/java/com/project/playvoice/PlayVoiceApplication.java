0115fe1eed810d6ded80f45ed7c12729070b6b81
wjdals898 <jungmin898@naver.com>
2024-01-01 15:41:39 +0900

    Initial commit

---
+++ src/main/java/com/project/playvoice/PlayVoiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.playvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayVoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayVoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/playvoice/PlayVoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.playvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayVoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
